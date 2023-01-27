20075b6455370d93bfbf1d9cc27cca7215ff15df
SooJi <sokoooool86@gmail.com>
2023-01-27 15:05:26 +0900

    refactor: ViewSchedule 모델에 id 속성 초기화 수정

--- PersonalScheduler/Presentation/ScheduleList/ViewSchedule.swift
+++ PersonalScheduler/Presentation/ScheduleList/ViewSchedule.swift
@@ -8,37 +8,39 @@
 import Foundation
 
 struct ViewSchedule {
+    let id: String
     let title: String?
     let startDate: String?
     let endDate: String?
     let content: String?
     let isCurrentDate: Bool?
-    let uuid: String // id 변경 못하게 수정
     
     init(_ data: [String : Any]) {
+        self.id = data["id"] as? String ?? "id"
         self.title = data["title"] as? String
         self.startDate = data["startDate"] as? String
         self.endDate = data["endDate"] as? String
         self.content = data["content"] as? String
         self.isCurrentDate = data["isCurrentDate"] as? Bool
-        self.uuid = data["uuid"] as? String ?? UUID().uuidString
     }
     
-    init(title: String,
+    init(id: String,
+         title: String,
          startDate: String,
          endDate: String,
          content: String,
          isCurrentDate: Bool = false) {
+        self.id = id
         self.title = title
         self.startDate = startDate
         self.endDate = endDate
         self.content = content
         self.isCurrentDate = isCurrentDate
-        self.uuid = UUID().uuidString
     }
     
     func toData() -> [String: Any] {
         var data = [String : Any]()
+        data["id"] = self.id
         data["title"] = self.title
         data["starDate"] = self.startDate
         data["endDate"] = self.endDate
