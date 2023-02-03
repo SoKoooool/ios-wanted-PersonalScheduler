294c8932079815e7a039f0c7881a4e13bae57278
SooJi <sokoooool86@gmail.com>
2023-02-03 14:53:45 +0900

    refactor: FCM 서버 요청 request 생성 메서드 수정

--- PersonalScheduler/Data/Service/FirebaseCloudMessagingService.swift
+++ PersonalScheduler/Data/Service/FirebaseCloudMessagingService.swift
@@ -13,20 +13,21 @@ final class FirebaseCloudMessagingService {
     private let fcmString = "https://fcm.googleapis.com/v1/projects/personalscheduler-2d82d/messages:send"
     
     func registerRemoteNotification(data: Data) {
-        guard let request = makeRequest(with: data) else { return }
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            
-        }.resume()
+        
     }
     
-    private func makeRequest(with data: Data) -> URLRequest? {
-        guard let url = URL(string: fcmString) else { return nil }
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        request.httpBody = data
-        return request
+    private func makeRequest(with data: Data, completion : @escaping (URLRequest) -> Void) {
+        getAccessToken { [weak self] token in
+            guard let token = token,
+                  let string = self?.fcmString,
+                  let url = URL(string: string) else { return }
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            request.httpBody = data
+            completion(request)
+        }
     }
     
     private func getAccessToken(completion: @escaping (String?) -> Void) {
